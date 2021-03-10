36fa1848a178448a6b44e09c9ce0bff69c4aaa29
zzz1109 <79627912+zzz1109@users.noreply.github.com>
2021-03-10 09:14:32 +0800

    Add files via upload

---
+++ week2-1.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n1=10,n2=20,n3=30;
+    printf("n1:%d n2:%d n3:%d\n",n1,n2,n3);
+
+    int *p=&n1;
+    *p=200;
+    printf("n1:%d n2:%d n3:%d\n",n1,n2,n3);
+    return 0;
+}
---
+++ week2-2.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n1=10,n2=20,n3=30;
+    printf("n1:%d n2:%d n3:%d\n",n1,n2,n3);
+
+    int *p=&n1;
+    *p=200;
+    printf("n1:%d n2:%d n3:%d\n",n1,n2,n3);
+
+    int *p2=&n3;
+    *p2=300;
+    printf("n1:%d n2:%d n3:%d\n",n1,n2,n3);
+
+    return 0;
+}
